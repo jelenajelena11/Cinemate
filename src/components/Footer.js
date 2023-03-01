216fb6fe93574ef55790df40a31603f4cbf5c687
unknown <stojanovicj97@gmail.com>
2023-03-01 11:13:15 +0100

    Added readme file

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,32 +1,44 @@
+import { Link } from "react-router-dom";
+
 export const Footer = () => {
   return (
     <footer className="p-4 bg-white shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
       <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-        © 2023{" "}
-        <a href="https://flowbite.com/" className="hover:underline">
+        © 2023
+        <Link to="/" className="hover:underline">
           Cinemate
-        </a>
+        </Link>
         . All Rights Reserved.
       </span>
       <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
-          <a href="/" className="mr-4 hover:underline md:mr-6 ">
-            About
+          <a
+            href="https://www.linkedin.com/in/jelena-stojanovic88/"
+            target="_blank"
+            rel="noreferrer"
+            className="mr-4 hover:underline md:mr-6 "
+          >
+            LinkedIn
           </a>
         </li>
         <li>
-          <a href="/" className="mr-4 hover:underline md:mr-6">
-            Privacy Policy
+          <a
+            href="https://github.com/jelenajelena11"
+            className="mr-4 hover:underline md:mr-6"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
           </a>
         </li>
         <li>
-          <a href="/" className="mr-4 hover:underline md:mr-6">
-            Licensing
-          </a>
-        </li>
-        <li>
-          <a href="/" className="hover:underline">
-            Contact
+          <a
+            href="https://gitlab.com/jelenas24"
+            className="mr-4 hover:underline md:mr-6"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Gitlab
           </a>
         </li>
       </ul>
